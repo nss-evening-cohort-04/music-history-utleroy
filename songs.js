587d8bffd1d5eafc1c7f17a63508b1056fa88167
Bryan Hankins <bryanhankins@me.com>
2016-07-28 21:39:55 -0500

    dynamic data displaying

--- songs.js
+++ songs.js
@@ -1,6 +1,8 @@
 var songs = [];
 var output = document.getElementById("output");
 
+
+
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
@@ -8,7 +10,7 @@ songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album App
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
 for (var i = 0; i < songs.length; i++) {
-  output.innerHTML += "<div>" + songs[i] + "</div"
+  output.innerHTML += "<h2>Song Name</h2><div>" + songs[i] + "</div>"
 }
 
 console.log(output.innerHTML)
