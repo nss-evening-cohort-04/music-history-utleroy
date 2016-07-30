5aef498e681afd24c72e64406d1a57986dea935a
Bryan Hankins <bryanhankins@me.com>
2016-07-30 16:16:29 -0500

    songs displaying to dom

--- songs.js
+++ songs.js
@@ -1,31 +1,34 @@
 var songs = [];
 var output = document.getElementById("output");
 
-
-
-
 songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
 songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
-
-for (var i = 0; i < songs.length; i++) {
-  output.innerHTML += "<h2>Song Name</h2>" + "<div>" + songs[i] + "</div>"
-}
-
-function remove(removed){
-  songs = [];
-  for (var i = 0; i < removed.length; i++) {
-    var newText = removed[i].replace(/[>]/g, "-")
-    songs.push(newText)
-    console.log(newText)
+function displaySongs () {
+  for (var i = 0; i < songs.length; i++) {
+    output.innerHTML += "<h2>Song Name</h2>" + "<div>" + songs[i] + "</div>"
   }
 }
-remove(songs)
 
-console.log()
+function removeChar(removed){
+  var fixedSongs = [];
+  for (var i = 0; i < removed.length; i++) {
+    var newText = removed[i].replace(/[>]/g, "-").replace(/[*,@,!,(]/g, "")
+    fixedSongs.push(newText)
+    console.log(newText)
+  }
+  return fixedSongs;
+}
+
+output.innerHTML += "<h2>Song Name</h2>" + "<div>" + removeChar(songs) + "</div>"
+
+
+
+// output.innerHTML += newOutput
+
 
 
 
